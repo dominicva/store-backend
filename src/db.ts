dae6979e06c14b6805abe6863d47ab180ac63e74
Dominic <dominicvana@gmail.com>
2021-09-03 19:32:22 +0200

    Feat: db config

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,15 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
+dotenv.config();
+
+const { PG_HOST, PG_DB, PG_USER, PG_PASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PG_HOST,
+  database: PG_DB,
+  user: PG_USER,
+  password: PG_PASSWORD,
+});
+
+export default pool;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 import morgan from 'morgan';
 
@@ -8,5 +8,9 @@ const app = express();
 
 app.use(json());
 app.use(morgan('dev'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world');
+});
 
 app.listen(3000, () => console.log(`Server running on port ${PORT}`));
