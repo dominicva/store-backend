59b51d98876862cb0cfce4384139a66a4ca5387e
Dominic <dominicvana@gmail.com>
2021-09-08 10:30:01 +0200

    Refactor: unused code in crud.ts

--- src/utils/crud.ts
+++ src/utils/crud.ts
@@ -1,4 +1,9 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { TOKEN_SECRET } = process.env;
 
 function getAll(model: any) {
   return async function getAllController(
@@ -19,10 +24,10 @@ function createOne(model: any) {
   return async function createOneController(req: Request, res: Response) {
     try {
       const data = req.body;
-      console.log('data:', data);
       const newResource = await model.create(data);
-      console.log(newResource);
-      res.status(201).json({ data: newResource });
+      const token = jwt.sign({ resource: newResource }, TOKEN_SECRET);
+      console.log(token);
+      res.status(201).json(token);
     } catch (err) {
       console.error(
         `Error creating new resource in ${model.tableName} table :: ${err}`
@@ -49,152 +54,3 @@ function showOne(model: any) {
 }
 
 export { getAll, createOne, showOne };
-// import client from '../db';
-// // import { Product } from '../resources/product/product.model';
-// // import { User } from '../resources/user/user.model';
-// export const getOne = (model) => async (req, res) => {
-//   try {
-//     const doc = await model
-//       .findOne({ createdBy: req.user._id, _id: req.params.id })
-//       .lean()
-//       .exec();
-
-//     if (!doc) {
-//       return res.status(400).end();
-//     }
-
-//     res.status(200).json({ data: doc });
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).end();
-//   }
-// };
-
-// export const getMany = (model) => async (req, res) => {
-//   try {
-//     const docs = await model.find({ createdBy: req.user._id }).lean().exec();
-
-//     res.status(200).json({ data: docs });
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).end();
-//   }
-// };
-
-// export const createOne = (model) => async (req, res) => {
-//   const createdBy = req.user._id;
-//   try {
-//     const doc = await model.create({ ...req.body, createdBy });
-//     res.status(201).json({ data: doc });
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).end();
-//   }
-// };
-
-// export const updateOne = (model) => async (req, res) => {
-//   try {
-//     const updatedDoc = await model
-//       .findOneAndUpdate(
-//         {
-//           createdBy: req.user._id,
-//           _id: req.params.id,
-//         },
-//         req.body,
-//         { new: true }
-//       )
-//       .lean()
-//       .exec();
-
-//     if (!updatedDoc) {
-//       return res.status(400).end();
-//     }
-
-//     res.status(200).json({ data: updatedDoc });
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).end();
-//   }
-// };
-
-// export const removeOne = (model) => async (req, res) => {
-//   try {
-//     const removed = await model.findOneAndRemove({
-//       createdBy: req.user._id,
-//       _id: req.params.id,
-//     });
-
-//     if (!removed) {
-//       return res.status(400).end();
-//     }
-
-//     return res.status(200).json({ data: removed });
-//   } catch (e) {
-//     console.error(e);
-//     res.status(400).end();
-//   }
-// };
-
-// export const crudControllers = (model) => ({
-//   removeOne: removeOne(model),
-//   updateOne: updateOne(model),
-//   getMany: getMany(model),
-//   getOne: getOne(model),
-//   createOne: createOne(model),
-// });
-
-// // class Crud<ModelType> {
-// //   async index(): Promise<ModelType[]> {
-// //     try {
-// //       const conn = await client.connect();
-// //       const sql = 'SELECT * FROM users';
-
-// //       const result = await conn.query(sql);
-
-// //       conn.release();
-// //       return result.rows;
-// //     } catch (error) {
-// //       throw new Error(`Cannot get users :: ${error}`);
-// //     }
-// //   }
-
-// //   async create(resource: ModelType): Promise<void> {
-// //     try {
-// //       const { firstName, lastName, password } = resource;
-// //       const conn = await client.connect();
-
-// //       const sql = `INSERT INTO users
-// //           (
-// //             first_name,
-// //             last_name,
-// //             password,
-// //           )
-// //           VALUES ($1, $2, $3)`;
-
-// //       await conn.query(sql, [firstName, lastName, password]);
-
-// //       conn.release();
-// //       return;
-// //     } catch (error) {
-// //       throw new Error(`Cannot add product to db :: ${error}`);
-// //     }
-// //   }
-
-// //   async show(id: string): Promise<User> {
-// //     try {
-// //       const conn = await client.connect();
-
-// //       const sql = 'SELECT * FROM users WHERE id=($1)';
-
-// //       const result = await conn.query(sql, [id]);
-
-// //       conn.release();
-
-// //       return result.rows[0];
-// //     } catch (err) {
-// //       throw new Error(`Could not find product ${id}. Error: ${err}`);
-// //     }
-// //   }
-// // }
-
-// // export default Crud;
